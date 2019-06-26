95e9343399d855ba50d409ea1c4a7de1ac0c8b35
tuble <iana.bratoeva@gmail.com>
2019-06-27 02:56:04 +0300

    Added bluetooth connection + code clean up

---
+++ Tank.ino
@@ -0,0 +1,106 @@
+#include <SoftwareSerial.h>
+
+char remote_data;
+
+SoftwareSerial Bluetooth(13, 12); // RX, TX on Arduino, connected to TX, RX on Bluetooth Module
+
+struct Track {
+  int pin1;
+  int pin2;
+  int enable;
+};
+
+void MoveTrack(struct Track, int, bool);
+
+struct Track left_track;
+struct Track right_track;
+
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("var init");
+
+
+  // left_track definition
+  left_track.pin1 = 9;
+  left_track.pin2 = 10;
+  left_track.enable = 11;
+
+  pinMode(left_track.pin1, OUTPUT);
+  pinMode(left_track.pin2, OUTPUT);
+  pinMode(left_track.enable, OUTPUT); // pwm?
+
+  //right_track definition
+  right_track.pin1 = 8;
+  right_track.pin2 = 7;
+  right_track.enable = 6;
+
+  pinMode(right_track.pin1, OUTPUT);
+  pinMode(right_track.pin2, OUTPUT);
+  pinMode(right_track.enable, OUTPUT); // pwm?
+
+  Bluetooth.begin(9600);
+  Bluetooth.println("Bluetooth module start");
+
+
+
+  MoveTrack(left_track, 255, true);
+}
+
+void loop() {
+
+  if (Bluetooth.available()) {
+
+    remote_data = Bluetooth.read();
+
+    if (remote_data == '0') {
+      Serial.println("stop left track");
+      MoveTrack(left_track, 0, false);
+    }
+    else if (remote_data == '1') {
+      Serial.println("left track forward");
+      MoveTrack(left_track, 255, true);
+    }
+    else if (remote_data == '2') {
+      Serial.println("left track backward");
+      MoveTrack(left_track, 255, false);
+    }
+    else if (remote_data == '3') {
+      Serial.println("stop right track");
+      MoveTrack(right_track, 0, false);
+    }
+    else if (remote_data == '4') {
+      Serial.println("right track forward");
+      MoveTrack(right_track, 255, true);
+    }
+    else if (remote_data == '5') {
+      Serial.println("right track backward");
+      MoveTrack(right_track, 255, false);
+    }
+
+  }
+
+}
+
+void MoveTrack(struct Track track, int speed_val, bool forward) {
+
+  if (speed_val == 0) // no movement
+  {
+    digitalWrite(track.pin1, LOW);
+    digitalWrite(track.pin2, LOW);
+    return; // because analogWrite?
+  }
+  else if (forward) // move track forwards
+  {
+    digitalWrite(track.pin1, LOW);
+    digitalWrite(track.pin2, HIGH);
+  }
+  else if (!forward) // move track backwards
+  {
+    digitalWrite(track.pin1, HIGH);
+    digitalWrite(track.pin2, LOW);
+  }
+
+  analogWrite(track.enable, speed_val); // no fucking idea
+}
